0144c56c6b7f429eb46b4da4d0966ebbb2a04623
yushi127 <proguradk@gmail.com>
2021-10-29 14:55:21 +0900

    動画、分析結果共にfirebaseに入れる

--- yushi/getmovie.py
+++ yushi/getmovie.py
@@ -6,33 +6,36 @@ from firebase_admin import credentials
 
 
 cred = credentials.Certificate('techinterview-a88b9-firebase-adminsdk-nd1jy-102817f0eb.json')
-
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'techinterview-a88b9.appspot.com.appspot.com'
 })
-# bucket = storage.bucket()
+
 class upload_all:
-    def __init__(self, uid,documentid,result):
+    def __init__(self, uid,documentid,result_movie,result_emotion):
         self.uid = uid; 
         self.documentid = documentid; 
-        self.result = result; 
+        self.result_movie = result_movie; 
+        self.result_emotion = result_emotion; 
 
 
     def upload_movie(self):
         db = firestore.client()
         doc_ref = db.collection('user').document(self.uid).collection('result').document(self.documentid)
         return doc_ref.update({
-            'movie_url': self.result
+            'movie_url': self.result_movie
             })
             
-    # def upload_result(uid,documentid,result):
-    #     db = firestore.client()
-    #     doc_ref = db.collection('user').document(uid).collection('result').document(documentid)
-    #     doc_ref.update({
-    #         'result': {kye: value},
-    #         })
+    def upload_result(self):
+        db = firestore.client()
+        doc_ref = db.collection('user').document(self.uid).collection('result').document(self.documentid)
+        doc_ref.update({
+            'result': result_emotion,
+            })
 
-# upload_result("q0REUe0hbYbacVskUlgVQKz1pd33","BJaroA3FiEJpO9JKjYqZ","aa","vv")
-upload_all("q0REUe0hbYbacVskUlgVQKz1pd33","BJaroA3FiEJpO9JKjYqZ","aaaaaaaa").upload_movie()
+result_emotion={"anger":0.07870,"contempt":0.078709,"disgust":0.078709,"fear":0.000000,"happiness":99.134199,"sadness":0.511610,"surprise":0.118064}
 
+# 呼び出し（引数＝ユーザーID、documentID、動画URL,分析結果の配列）
+upload_all("q0REUe0hbYbacVskUlgVQKz1pd33","BJaroA3FiEJpO9JKjYqZ","aaaaaa",result_emotion).upload_movie()
+
+upload_all("q0REUe0hbYbacVskUlgVQKz1pd33","BJaroA3FiEJpO9JKjYqZ","aaaaaa",result_emotion).upload_result()
 
